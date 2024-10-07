9f30bdc5d48de7a0698628f5688a07ba16ef2750
Timothée Demares <timothee.demares@francetravail.fr>
2024-10-07 16:50:19 +0200

    refactor: resize image

--- ecoimage/core/resizer.py
+++ ecoimage/core/resizer.py
@@ -1,21 +1,48 @@
 from pathlib import Path
+from typing import NamedTuple
 
 from PIL import Image
 
+from ecoimage.common.exc import EcoImageError
 
-def resize_image(img_path: Path | str, scale_factor: float) -> Path:
+
+class ImgData(NamedTuple):
+    path: Path
+    image: Image.Image
+    width: int
+    height: int
+
+
+def open_img(img_path: Path | str) -> ImgData:
     img_path = Path(img_path)
     img = Image.open(img_path)
 
-    original_width, original_height = img.size
-    new_width = int(original_width * scale_factor)
-    new_height = int(original_height * scale_factor)
+    return ImgData(img_path, img, *img.size)
 
-    resized_img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
 
-    new_path = img_path.with_name(
-        f"{img_path.stem}_{new_width}_{new_height}{img_path.suffix}"
+def resize_image(
+    img_data: ImgData,
+    scale_factor: float,
+    *,
+    output_folder: Path | str = None,
+) -> ImgData:
+    if output_folder is None:
+        output_folder = img_data.path.parent
+
+    if output_folder.is_dir is False:
+        raise EcoImageError(f"{output_folder} is not a directory")
+
+    new_width = int(img_data.width * scale_factor)
+    new_height = int(img_data.height * scale_factor)
+
+    resized_img = img_data.image.resize(
+        (new_width, new_height), Image.Resampling.LANCZOS
     )
-    resized_img.save(new_path, format=img.format)
 
-    return new_path
+    new_path = (
+        output_folder
+        / f"{img_data.path.stem}_{new_width}x{new_height}{img_data.path.suffix}"
+    )
+    resized_img.save(new_path, format=img_data.image.format)
+
+    return ImgData(new_path, resized_img, new_width, new_height)
--- tests/test_resize_image.py
+++ tests/test_resize_image.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
-from ecoimage.core.resizer import resize_image
+from ecoimage.core.resizer import open_img, resize_image
 
 
 def test_resize_image(jpg_test: Path):
-    resized_img = resize_image(jpg_test, scale_factor=0.5)
+    img_data = open_img(jpg_test)
+    factor = 0.5
+    resized_img = resize_image(img_data, scale_factor=factor)
 
-    assert resized_img.exists()
+    assert resized_img.path.exists()
+    assert img_data.width * factor - resized_img.width < 1
+    assert img_data.height * factor - resized_img.height < 1
