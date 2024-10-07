55f1b5286efa07907c524d9ac0a098a40115e4ae
Timothée Demares <timothee.demares@francetravail.fr>
2024-10-07 10:22:46 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+This file is the setup script for the package.
+"""
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt", mode="r", encoding="utf-8") as f:
+    requirements = f.readlines()
+
+setup(
+    name="ecodesign",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[lib.strip() for lib in requirements],
+)
